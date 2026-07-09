3541fcff7f0a9dc67bcddc73d24f98fd68d3721f
Nishchaymodi8 <nishchaymodi8@gmail.com>
2026-07-09 22:08:13 +0530

    Implement of GitHub OAuth

--- backend/github_integration/models.py
+++ backend/github_integration/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+import uuid
 class GitHubAccount(models.Model):
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL,
@@ -17,3 +17,22 @@ class GitHubAccount(models.Model):
 
     def __str__(self):
         return self.username
+    
+class OAuthState(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    state = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        editable=False
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.state}"
--- backend/github_integration/views.py
+++ backend/github_integration/views.py
@@ -7,26 +7,42 @@ from rest_framework.permissions import IsAuthenticated
 
 import requests
 
-from .models import GitHubAccount
+from .models import GitHubAccount,OAuthState
 
 class GitHubConnectView(APIView):
 
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
+
+        OAuthState.objects.filter(user=request.user).delete()
+
+        oauth_state = OAuthState.objects.create(
+            user=request.user
+        )
 
         github_url = (
             "https://github.com/login/oauth/authorize"
             f"?client_id={settings.GITHUB_CLIENT_ID}"
+            f"&state={oauth_state.state}"
         )
 
         return Response({
             "url": github_url
         })
-
+    
 class GitHubCallbackView(APIView):
 
     def get(self, request):
 
         code = request.GET.get("code")
+        state = request.GET.get("state")
+
+        oauth_state = OAuthState.objects.get(
+        state=state
+        )
+
+        user = oauth_state.user
 
         token_response = requests.post(
             "https://github.com/login/oauth/access_token",
@@ -51,14 +67,17 @@ class GitHubCallbackView(APIView):
 
         github_user = user_response.json()
 
-  #      GitHubAccount.objects.update_or_create(
-  #          github_id=str(github_user["id"]),
-  #          defaults={
-  #              "username": github_user["login"],
-  #              "access_token": access_token,
-   #             "user": ???,
-   #            }
-   #     )
-        
+        GitHubAccount.objects.update_or_create(
+            user=user,
+            defaults={
+                "github_id": str(github_user["id"]),
+                "username": github_user["login"],
+                "access_token": access_token,
+            }
+        )
+        oauth_state.delete()
 
-        return Response(github_user)    
+        return Response({
+                "message": "GitHub connected successfully",
+                "github_username": github_user["login"]
+            })
